b756940467fca53713f93ef7b5df13152431df92
Mariano Cuartero <cuarteroma@gmail.com>
2022-07-04 13:33:06 -0300

    1.0

---
+++ index.js
@@ -0,0 +1,304 @@
+// Se requerirá al usuario seleccionar entre una serie de opciones para definir su perfil inversor y cuales son los FCI que se adecúan al mismo
+
+const listaPreguntas = [
+{numero: 0,
+cantidad: 4,
+pregunta: "1 - Mi conocimiento sobre Mercado de Capitales es:\n 1) Nulo\n 2) Poco\n 3)  Mucho\n 4) Profesional"},
+{numero: 1,
+cantidad: 4,
+pregunta: "2 - He invertido en el Mercado de Capitales:\n 1) Nunca \n 2) Pocas veces y no me gustó\n 3) Mucho y ya conozco los riesgos\n 4) Siempre, lo hago habitualmente"},
+{numero: 2,
+cantidad: 4,
+pregunta: "3 - En caso de una baja en el valor de mis activos:\n 1) Rescataría todo asumiendo la pérdida \n 2) Rescataría una parte \n 3) Conservaría mis activos esperando a que suban, pese a que puedan bajar más\n 4) Aprovecharía la oportunidad y agregaría más capital"},
+{numero: 3,
+cantidad: 4,
+pregunta: "4 - El porcentaje de mis ingresos mensuales que destino a pago de deudas de préstamos, tarjetas de crédito, y demás, es:\n 1) Más del 50% \n 2) Entre un 26% y 50% \n 3) Entre el 11% y 25%\n 4) Menos del 10% "},
+{numero: 4,
+cantidad: 3,
+pregunta: "5 - Mi objetivo final es:\n 1) Mantener el valor de mi dinero con una rentabilidad mínima \n 2) Tener una ganancia superior a la de un Plazo Fijo, aunque se encuentre sujeta a variaciones del mercado \n 3) Obtener una ganancia significativa, corriendo el riesgo de perder más de la mitad de la inversión inicial"},
+{numero: 5,
+cantidad: 4,
+pregunta: "6 - Tengo a mi cargo:\n 1) Ninguna persona \n 2) 1 persona \n 3) 2 a 3 personas\n 4) Más de 3 personas "},
+{numero: 6,
+cantidad: 4,
+pregunta: "7 - La cantidad de mis ahorros que estoy dispuesto a invertir en el Mercado de Capitales es: \n 1) Menor al 25% \n 2) Entre el 26% y 50%\n 3) Entre el 51% y 75% \n 4) Más del 76%"},
+{numero: 7,
+cantidad: 4,
+pregunta: "8 - Necesitaré el dinero que invierto en:\n 1) Menos de 2 meses \n 2) En medio año \n 3) En un año \n 4) En más de un año "},
+{numero: 8,
+cantidad: 5,
+pregunta: "9 - Del total de mi dinero estoy dispuesto a asumir una pérdida de \n 1)Ninguna pérdida \n 2) Hasta un 10% \n 3) Hasta un 25%\n 4) Hasta un 50% \n 5) Más del 50%"},
+{numero: 9,
+cantidad: 3,
+pregunta: "10 - Me gustaría invertir \n 1) El total de mis Activos en Renta Fija a corto plazo (Bonos, Fonodos Comunes de Inversión, Fideiomisos Financieros) \n 2) La mitad de mis Activos en Renta Fija y el resto en ACCIONES, y demás Activos de Renta Variable \n 3) El total de mis Activos en Renta Variable "}
+];
+
+const listaResultados = ["Conservador", "Moderado", "Agresivo"];
+const Conservador = [
+    {nombre: "ADCAP PESOS PLUS Clase A",
+     plazo: "Corto Plazo",
+     inversionMinima: 1000,
+     moneda: "Pesos",
+     rescate: "48hs",
+     tipoRenta: "Renta Fija",
+     valorCuotaparte: 10.4625,
+     rentabilidad: {ultimoAño: "46%",
+                    ultimoMes: "3%",
+                    } },
+    
+    {nombre: "FIMA PREMIUM Clase A",
+    plazo: "Muy Corto Plazo",
+    inversionMinima: 1000,
+    moneda: "Pesos",
+    rescate: "Inmediato",
+    tipoRenta: "Renta Fija",
+    valorCuotaparte: 12.1322,
+    rentabilidad: {ultimoAño: "35.73%",
+                   ultimoMes: "3.19%",
+                   } },
+    {nombre: "GALILEO AHORRO Clase A",
+    plazo: "Muy Corto Plazo",
+    inversionMinima: 1000,
+    moneda: "Pesos",
+    rescate: "24hs",
+    tipoRenta: "Renta Fija",
+    valorCuotaparte: 24.4339,
+    rentabilidad: {ultimoAño: "47.90%",
+                   ultimoMes: "4.42%",
+                   } },
+    {nombre: "TORONTO TRUST AHORRO Clase A",
+    plazo: "Corto Plazo",
+    inversionMinima: 1000,
+    moneda: "Pesos",
+    rescate: "Inmediato",
+    tipoRenta: "Money Market",
+    valorCuotaparte: 7.3082,
+    rentabilidad: {ultimoAño: "37.23%",
+                   ultimoMes: "3.29%",
+                   } },
+    {nombre: "TORONTO TRUST LIQUIDEZ DÓLAR Clase A - Divisa",
+    plazo: "Corto Plazo",
+    inversionMinima: 1000,
+    moneda: "Dólares",
+    rescate: "48hs",
+    tipoRenta: "Renta Fija",
+    valorCuotaparte: 1.0731,
+    rentabilidad: {ultimoAño: "-0.24%",
+                   ultimoMes: "-.05%",
+                   } },];
+
+const Moderado = [
+    {nombre: "ADCAP PESOS PLUS Clase A",
+    plazo: "Corto Plazo",
+    inversionMinima: 1000,
+    moneda: "Pesos",
+    rescate: "48hs",
+    tipoRenta: "Renta Fija",
+    valorCuotaparte: 10.4625,
+    rentabilidad: {ultimoAño: "46%",
+                   ultimoMes: "3%",
+                   } },
+   
+   {nombre: "FIMA PREMIUM Clase A",
+   plazo: "Muy Corto Plazo",
+   inversionMinima: 1000,
+   moneda: "Pesos",
+   rescate: "Inmediato",
+   tipoRenta: "Renta Fija",
+   valorCuotaparte: 12.1322,
+   rentabilidad: {ultimoAño: "35.73%",
+                  ultimoMes: "3.19%",
+                  } },
+   {nombre: "GALILEO AHORRO Clase A",
+   plazo: "Muy Corto Plazo",
+   inversionMinima: 1000,
+   moneda: "Pesos",
+   rescate: "24hs",
+   tipoRenta: "Renta Fija",
+   valorCuotaparte: 24.4339,
+   rentabilidad: {ultimoAño: "47.90%",
+                  ultimoMes: "4.42%",
+                  } },
+   {nombre: "TORONTO TRUST AHORRO Clase A",
+   plazo: "Corto Plazo",
+   inversionMinima: 1000,
+   moneda: "Pesos",
+   rescate: "Inmediato",
+   tipoRenta: "Money Market",
+   valorCuotaparte: 7.3082,
+   rentabilidad: {ultimoAño: "37.23%",
+                  ultimoMes: "3.29%",
+                  } },
+   {nombre: "TORONTO TRUST LIQUIDEZ DÓLAR Clase A - Divisa",
+   plazo: "Corto Plazo",
+   inversionMinima: 1000,
+   moneda: "Dólares",
+   rescate: "48hs",
+   tipoRenta: "Renta Fija",
+   valorCuotaparte: 1.0731,
+   rentabilidad: {ultimoAño: "-0.24%",
+                  ultimoMes: "-.05%",
+                  } },];
+const Agresivo = [
+    {nombre: "ADCAP PESOS PLUS Clase A",
+    plazo: "Corto Plazo",
+    inversionMinima: 1000,
+    moneda: "Pesos",
+    rescate: "48hs",
+    tipoRenta: "Renta Fija",
+    valorCuotaparte: 10.4625,
+    rentabilidad: {ultimoAño: "46%",
+                   ultimoMes: "3%",
+                   } },
+   
+   {nombre: "FIMA PREMIUM Clase A",
+   plazo: "Muy Corto Plazo",
+   inversionMinima: 1000,
+   moneda: "Pesos",
+   rescate: "Inmediato",
+   tipoRenta: "Renta Fija",
+   valorCuotaparte: 12.1322,
+   rentabilidad: {ultimoAño: "35.73%",
+                  ultimoMes: "3.19%",
+                  } },
+   {nombre: "GALILEO AHORRO Clase A",
+   plazo: "Muy Corto Plazo",
+   inversionMinima: 1000,
+   moneda: "Pesos",
+   rescate: "24hs",
+   tipoRenta: "Renta Fija",
+   valorCuotaparte: 24.4339,
+   rentabilidad: {ultimoAño: "47.90%",
+                  ultimoMes: "4.42%",
+                  } },
+   {nombre: "TORONTO TRUST AHORRO Clase A",
+   plazo: "Corto Plazo",
+   inversionMinima: 1000,
+   moneda: "Pesos",
+   rescate: "Inmediato",
+   tipoRenta: "Money Market",
+   valorCuotaparte: 7.3082,
+   rentabilidad: {ultimoAño: "37.23%",
+                  ultimoMes: "3.29%",
+                  } },
+   {nombre: "TORONTO TRUST LIQUIDEZ DÓLAR Clase A - Divisa",
+   plazo: "Corto Plazo",
+   inversionMinima: 1000,
+   moneda: "Dólares",
+   rescate: "48hs",
+   tipoRenta: "Renta Fija",
+   valorCuotaparte: 1.0731,
+   rentabilidad: {ultimoAño: "-0.24%",
+                  ultimoMes: "-.05%",
+                  } },];
+let resultadoFinal = 0;
+let opcionSuma = [];
+let perfilCalculado;
+
+function sumarOpciones(nroPregunta){
+    do {
+        opcionSuma[nroPregunta] = parseInt(prompt(listaPreguntas[nroPregunta].pregunta));
+        if (opcionSuma[nroPregunta] < 1 || opcionSuma[nroPregunta] > listaPreguntas[nroPregunta].cantidad || isNaN(opcionSuma[nroPregunta]))
+            {
+            alert("Debe ingresar el número de opción correspondiente");
+            }
+        } while (opcionSuma[nroPregunta] < 1 || opcionSuma[nroPregunta] > listaPreguntas[nroPregunta].cantidad || isNaN(opcionSuma[nroPregunta]));
+}
+function calcularResultadoFinal(suma){
+    return suma.reduce( (x,y) => x + y, 0);
+}
+function opcionesResultadoFinal(valor){
+    let resultadosParaMostrar;
+    if (valor <= 13) {
+        perfilCalculado = listaResultados[0];
+        resultadosParaMostrar = Conservador;
+    } else if (valor <= 26) {
+        perfilCalculado = listaResultados[1];
+        resultadosParaMostrar = Moderado;
+        
+    } else {
+        perfilCalculado = listaResultados[2]
+        resultadosParaMostrar = Agresivo;
+    }
+    alert(`${nombreUsuario}, según las opciones indicadas tu perfil corresponde al Perfil ${perfilCalculado}`);
+    alert(`Las opciones de inversión para el Perfil ${perfilCalculado}, son las siguientes: `);
+    for (const recorrerArray in resultadosParaMostrar) {
+        for (const recorrerObjeto in resultadosParaMostrar[recorrerArray]){
+            document.write(`${recorrerObjeto}: `);
+            document.write(`${JSON.stringify(resultadosParaMostrar[recorrerArray][recorrerObjeto])}`);
+            document.write("<br>");
+        }
+        document.write("<br>");
+        document.write("<br>");        
+    }
+
+}
+
+alert("----------------------TEST INICIAL DEL INVERSOR---------------------- \nDescubramos que tipo de inversor sos y que productos se ajustan a tus necesidades");
+
+do {
+    nombreUsuario = prompt("Ingrese su nombre");
+    if (nombreUsuario == ""){
+        alert("Debe ingresar un nombre válido");
+    }
+} while (nombreUsuario == "");
+
+alert(`${nombreUsuario} te plantearemos 10 situaciones en las que deberás indicar la opción que más se adecúe a tu perfil:`)
+
+for (const recorrido in listaPreguntas){
+    sumarOpciones(recorrido);
+};
+
+resultadoFinal= calcularResultadoFinal(opcionSuma);
+opcionesResultadoFinal(resultadoFinal);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* opcion[0] = parseInt(prompt("1 - Mi conocimiento sobre Mercado de Capitales es:\n 1) Nulo\n 2) Poco\n 3)  Mucho\n 4) Profesional"));
+
+opcion[1] = parseInt(prompt("2 - He invertido en el Mercado de Capitales:\n 1) Nunca \n 2) Pocas veces y no me gustó\n 3) Mucho y ya conozco los riesgos\n 4) Siempre, lo hago habitualmente "));
+
+opcion[2] = parseInt(prompt("3 - En caso de una baja en el valor de mis activos:\n 1) Rescataría todo asumiendo la pérdida \n 2) Rescataría una parte \n 3) Conservaría mis activos esperando a que suban, pese a que puedan bajar más\n 4) Aprovecharía la oportunidad y agregaría más capital "));
+
+opcion4 = parseInt(prompt("4 - El porcentaje de mis ingresos mensuales que destino a pago de deudas de préstamos, tarjetas de crédito, y demás, es:\n 1) Más del 50% \n 2) Entre un 26% y 50% \n 3) Entre el 11% y 25%\n 4) Menos del 10% "));
+
+opcion5 = parseInt(prompt("5 - Mi objetivo final es:\n 1) Mantener el valor de mi dinero con una rentabilidad mínima \n 2) Tener una ganancia superior a la de un Plazo Fijo, aunque se encuentre sujeta a variaciones del mercado \n 3) Obtener una ganancia significativa, corriendo el riesgo de perder más de la mitad de la inversión inicial"));
+
+opcion6 = parseInt(prompt("6 - Tengo a mi cargo:\n 1) Ninguna persona \n 2) 1 persona \n 3) 2 a 3 personas\n 4) Más de 3 personas "));
+
+opcion7 = parseInt(prompt("7 - La cantidad de mis ahorros que estoy dispuesto a invertir en el Mercado de Capitales es: \n 1) Menor al 25% \n 2) Entre el 26% y 50%\n 3) Entre el 51% y 75% \n 4) Más del 76%"));
+
+opcion8 = parseInt(prompt("8 - Necesitaré el dinero que invierto en:\n 1) Menos de 2 meses \n 2) En medio año \n 3) En un año \n 4) En más de un año "));
+
+opcion9 = parseInt(prompt("9 - Del total de mi dinero estoy dispuesto a asumir una pérdida de \n 1)Ninguna pérdida \n 2) Hasta un 10% \n 3) Hasta un 25%\n 4) Hasta un 50% \n 5) Más del 50%"));
+
+opcion10 = parseInt(prompt("10 - Me gustaría invertir \n 1) El total de mis Activos en Renta Fija a corto plazo (Bonos, Fonodos Comunes de Inversión, Fideiomisos Financieros) \n 2) La mitad de mis Activos en Renta Fija y el resto en ACCIONES, y demás Activos de Renta Variable \n 3) El total de mis Activos en Renta Variable ")); */
+
