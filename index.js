043d24bbe47f0d9859d9ec546c06b2ae670af247
Mariano Ariel Cuartero <cuarteroma@gmail.com>
2022-07-11 13:57:26 -0300

    Proyecto Final JS 2.1 Primer Entrega Proyecto Final  - Añadiendo filtros y busqueda

--- index.js
+++ index.js
@@ -51,23 +51,23 @@ class FondoDeInversion {
 const listaPerfiles = ["Conservador", "Moderado", "Agresivo"];
 const listaFondos = [];
 
-listaFondos.push(new FondoDeInversion("ADCAP PESOS PLUS Clase A","Corto Plazo", 1000, "Pesos", "48hs", "Renta Fija", 10.4625, "46%", "3%", "Conservador"));
-listaFondos.push(new FondoDeInversion("FIMA PREMIUM Clase A", "Muy Corto Plazo", 1000, "Pesos", "Inmediato", "Renta Fija", 12.1322, "35.73%", "3.19%", "Conservador"));
-listaFondos.push(new FondoDeInversion("GALILEO AHORRO Clase A", "Muy Corto Plazo", 1000, "Pesos", "24hs", "Renta Fija", 24.4339, "47.90%", "4.42%", "Conservador"));
-listaFondos.push(new FondoDeInversion("TORONTO TRUST AHORRO Clase A", "Corto Plazo", 1000, "Pesos", "Inmediato", "Money Market", 7.3082, "37.23%", "3.29%", "Conservador"));
-listaFondos.push(new FondoDeInversion("TORONTO TRUST LIQUIDEZ DÓLAR Clase A - Divisa", "Corto Plazo", 1000, "Dólares", "48hs", "Renta Fija", 1.0731, "-0.24%", "-.05%", "Conservador"));
+listaFondos.push(new FondoDeInversion("ADCAP PESOS PLUS Clase A","corto", 1000, "pesos", "48hs", "Renta Fija", 10.4625, 46, 3, "Conservador"));
+listaFondos.push(new FondoDeInversion("FIMA PREMIUM Clase A", "corto", 1000, "pesos", "Inmediato", "Renta Fija", 12.1322, 35.73, 3.19, "Conservador"));
+listaFondos.push(new FondoDeInversion("GALILEO AHORRO Clase A", "corto", 1000, "pesos", "24hs", "Renta Fija", 24.4339, 47.90, 4.42, "Conservador"));
+listaFondos.push(new FondoDeInversion("TORONTO TRUST AHORRO Clase A", "corto", 1000, "pesos", "Inmediato", "Money Market", 7.3082, 37.23, 3.29, "Conservador"));
+listaFondos.push(new FondoDeInversion("TORONTO TRUST LIQUIDEZ DÓLAR Clase A - Divisa", "corto", 1000, "dolares", "48hs", "Renta Fija", 1.0731, -0.24, -0.05, "Conservador"));
 
-listaFondos.push(new FondoDeInversion("ADCAP BALANCEADO II Clase A", "Corto Plazo", 1000, "Pesos", "48hs", "Renta Fija", 2.0810, "49.04%", "-0.86%", "Moderado"));
-listaFondos.push(new FondoDeInversion("ADCAP RENTA FIJA ARGENTINA Clase A", "Corto Plazo",  1000, "Pesos", "48hs", "Dolar Linked", 3.4528, "32.40%", "2.67%", "Moderado"));
-listaFondos.push(new FondoDeInversion("ADCAP RETORNO TOTAL Clase A", "Corto Plazo", 1000, "Pesos", "24hs", "Renta Fija", 24.4339, "47.90%", "4.42%", "Moderado"));
-listaFondos.push(new FondoDeInversion("ALLARIA COBERTURA DINÁMICA Clase A", "Mediano Plazo", 1000, "Pesos", "48hs", "Renta Fija", 1.3935, "39.35%", "1.3935%", "Moderado"));
-listaFondos.push(new FondoDeInversion("ARGENFUNDS ABIERTO PYMES Clase B", "Mediano Plazo", 10000, "Pesos", "48hs", "Renta Fija", 3.2272, "36.64%", "2.14%", "Moderado"));
+listaFondos.push(new FondoDeInversion("ADCAP BALANCEADO II Clase A", "corto", 1000, "pesos", "48hs", "Renta Fija", 2.0810, 49.04, -0.86, "Moderado"));
+listaFondos.push(new FondoDeInversion("ADCAP RENTA FIJA ARGENTINA Clase A", "corto",  1000, "pesos", "48hs", "Dolar Linked", 3.4528, 32.40, 2.67, "Moderado"));
+listaFondos.push(new FondoDeInversion("ADCAP RETORNO TOTAL Clase A", "corto", 1000, "pesos", "24hs", "Renta Fija", 24.4339, 47.90, 4.42, "Moderado"));
+listaFondos.push(new FondoDeInversion("ALLARIA COBERTURA DINÁMICA Clase A", "mediano", 1000, "pesos", "48hs", "Renta Fija", 1.3935, 39.35, 1.3935, "Moderado"));
+listaFondos.push(new FondoDeInversion("ARGENFUNDS ABIERTO PYMES Clase B", "mediano", 10000, "pesos", "48hs", "Renta Fija", 3.2272, 36.64, 2.14, "Moderado"));
 
-listaFondos.push(new FondoDeInversion("ADCAP BALANCEADO Clase A", "Mediano/Largo", 500, "Pesos", "48hs", "Retorno Total", 6.4304, "39.52%", "2.06%", "Agresivo"));
-listaFondos.push(new FondoDeInversion("ALLARIA ACCIONES Clase A", "Largo Plazo", 1000, "Pesos", "48hs", "Renta Variable", 33.7802, "53.68%", "13.41%", "Agresivo"));
-listaFondos.push(new FondoDeInversion("TORONTO TRUST ARGENTINA 2021 Clase A", "Mediano/Largo Plazo", 1000, "Pesos", "48hs", "Renta Mixta", 24.4339, "1.85%", "2.03%", "Agresivo"));
-listaFondos.push(new FondoDeInversion("BALANZ ACCIONES Clase B", "Largo Plazo", 20000, "Pesos", "48hs", "Renta Variable", 5.64, "13.56%", "57.13%", "Agresivo"));
-listaFondos.push(new FondoDeInversion("CMA ACCIONES Clase A", "Largo Plazo", 2000, "Pesos", "48hs", "Renta Variable", 3.2251, "51.21%", "10.94%", "Agresivo"));
+listaFondos.push(new FondoDeInversion("ADCAP BALANCEADO Clase A", "largo", 500, "pesos", "48hs", "Retorno Total", 6.4304, 39.52, 2.06, "Agresivo"));
+listaFondos.push(new FondoDeInversion("ALLARIA ACCIONES Clase A", "largo", 1000, "pesos", "48hs", "Renta Variable", 33.7802, 53.68, 13.41, "Agresivo"));
+listaFondos.push(new FondoDeInversion("TORONTO TRUST ARGENTINA 2021 Clase A", "largo", 1000, "pesos", "48hs", "Renta Mixta", 24.4339, 1.85, 2.03, "Agresivo"));
+listaFondos.push(new FondoDeInversion("BALANZ ACCIONES Clase B", "largo", 20000, "pesos", "48hs", "Renta Variable", 5.64, 13.56, 57.13, "Agresivo"));
+listaFondos.push(new FondoDeInversion("CMA ACCIONES Clase A", "largo", 2000, "pesos", "48hs", "Renta Variable", 3.2251, 51.21, 10.94, "Agresivo"));
 
 let opcionSuma = [];
 
@@ -125,6 +125,7 @@ function opcionesResultadoFinal(resultadoFinal){
         }
         document.write("<br><br>");      */ 
     }
+    document.write(`<br><br>`);
 
 }
 
@@ -145,4 +146,53 @@ for (const recorrido in listaPreguntas){
 
 opcionesResultadoFinal(calcularResultadoFinal(opcionSuma));
 
+//FILTROS Y BUSQUEDA
 
+function buscarMinimoInversion (valor){
+    let resultadosParaMostrar = [];
+    resultadosParaMostrar = listaFondos.filter(elemento => elemento.inversionMinima <= valor);
+    document.write(`Se muestran ${resultadosParaMostrar.length} resultados por debajo de ${valor} Pesos/Dolares de inversión mínima: <br><br>`)
+    for (const recorrerArray in resultadosParaMostrar) {
+        document.write(`- ${resultadosParaMostrar[recorrerArray].nombre}<br>`);
+        document.write(`- Inversión minima ${resultadosParaMostrar[recorrerArray].inversionMinima} ${resultadosParaMostrar[recorrerArray].moneda}<br><br>`);
+    };
+};
+
+buscarMinimoInversion (prompt("Ingrese el monto mínimo a invertir deseado: "))
+
+function filtrarMayorRentabilidadAnio () {
+    let resultadosParaMostrar = [];
+    resultadosParaMostrar = listaFondos.map(elemento => elemento);
+    resultadosParaMostrar.sort((a,b) => b.rentabilidad.ultimoAño - a.rentabilidad.ultimoAño);
+    document.write(`Se muestran ${resultadosParaMostrar.length} resultados ordenados por mayor nivel de rentabilidad.<br><br>`)
+    for (const recorrerArray in resultadosParaMostrar) {
+        document.write(`- ${resultadosParaMostrar[recorrerArray].nombre}<br>`);
+        document.write(`- Rentabilidad en el último año ${resultadosParaMostrar[recorrerArray].rentabilidad.ultimoAño}%<br><br>`);
+    };
+};
+
+filtrarMayorRentabilidadAnio();
+
+function filtrarMenorPlazo (valor){
+    let resultadosParaMostrar = [];
+    resultadosParaMostrar = listaFondos.filter(elemento => elemento.plazo == valor);
+    document.write(`Se muestran ${resultadosParaMostrar.length} resultados para inversiones de ${valor} lazo: <br><br>`)
+    for (const recorrerArray in resultadosParaMostrar) {
+        document.write(`- ${resultadosParaMostrar[recorrerArray].nombre}<br>`);
+        document.write(`- ${resultadosParaMostrar[recorrerArray].plazo} plazo.<br><br>`);
+    };
+
+};
+
+filtrarMenorPlazo(prompt("Ingrese el plazo de la inversion deseado (Corto/Mediano/Largo").toLowerCase());
+
+function buscarMoneda (valor){
+    let resultadosParaMostrar = [];
+    resultadosParaMostrar = listaFondos.filter(elemento => elemento.moneda == valor);
+    document.write(`Se muestran ${resultadosParaMostrar.length} resultados en ${valor}.<br><br>`)
+    for (const recorrerArray in resultadosParaMostrar) {
+        document.write(`- ${resultadosParaMostrar[recorrerArray].nombre}<br>`);
+        document.write(`- ${resultadosParaMostrar[recorrerArray].moneda}.<br><br>`);
+    };
+}
+buscarMoneda(prompt("Ingrese la moneda a filtrar (pesos / dolares)").toLowerCase())
